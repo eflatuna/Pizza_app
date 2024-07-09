8d734a59175177e728f1bfd4d50968302611b610
esra <esrabaskaya84@gmail.com>
2024-07-09 10:42:51 +0200

    "Updated authentication middleware to support both Token and Bearer authentication schemes"

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -13,12 +13,18 @@ module.exports = async (req, res, next) => {
 	const auth = req.headers?.authorization;
 	const tokenKey = auth ? auth.split(" ") : null;
 
+	// if (tokenKey && tokenKey[0] == "Token") {
+	// 	//* "Token asdaırhıawerasd...""
+	// 	const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+	// 		"userId"
+	// 	);
+	// 	req.user = tokenData ? tokenData.userId : false;
+	// }
 	if (tokenKey && tokenKey[0] == "Token") {
 		//* "Token asdaırhıawerasd...""
 		const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
 			"userId"
 		);
 		req.user = tokenData ? tokenData.userId : false;
-	}
-	next();
+	} else if (tokenKey[0] == "Bearer") next();
 };
